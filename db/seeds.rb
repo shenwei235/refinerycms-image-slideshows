f08af01e16cac66e0f82ff45fc9fd285639c4376
shenwei235 <shenwei235@gmail.com>
2014-12-01 16:45:29 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-(Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
+[:en].each do |lang|
   I18n.locale = lang
 
   if defined?(Refinery::User)
@@ -12,7 +12,7 @@
 
 end
 
-(Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
+[:en].each do |lang|
   I18n.locale = lang
 
   if defined?(Refinery::User)
